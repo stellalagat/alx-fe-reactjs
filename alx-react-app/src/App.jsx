e0406fac1d10ca43fd7f4be6cba0cebbc1271cd4
Harriet Jeruto <stellalagat058@gmail.com>
2025-08-31 01:48:39 +0300

    imports

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,9 +3,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from "./Header";
-import MainContent from "./MainContent";
-import Footer from "./Footer";
+
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +18,12 @@ function App() {
     <Header />
       <MainContent />
       <Footer />
+      <h1>User Profile Card</h1>
+      <UserProfile 
+        name="Alice Johnson" 
+        age="25" 
+        bio="Loves hiking, photography, and building cool apps with React." 
+      />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
