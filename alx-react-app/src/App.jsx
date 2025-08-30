fac26ca8611c2d1b3773b34a831d117bec3fb03a
Harriet Jeruto <stellalagat058@gmail.com>
2025-08-31 01:21:04 +0300

    diff components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +21,9 @@ function App() {
         </a>
       </div>
        <WelcomeMessage />
+        <Header />
+      <MainContent />
+      <Footer />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
