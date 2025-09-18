ed7bbddf6c9ae868c17d020ef41bf5ff1a72a9f4
stellalagat <stellalagat058@gmail.com>
2025-09-18 14:47:30 +0300

    recipejs

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,15 +2,27 @@ import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: "",
+
   addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+    })),
+
   updateRecipe: (id, updatedFields) =>
     set((state) => ({
       recipes: state.recipes.map((r) =>
         r.id === id ? { ...r, ...updatedFields } : r
       ),
     })),
+
   deleteRecipe: (id) =>
-    set((state) => ({ recipes: state.recipes.filter((r) => r.id !== id) })),
+    set((state) => ({
+      recipes: state.recipes.filter((r) => r.id !== id),
+    })),
+
   setRecipes: (recipes) => set({ recipes }),
+
+  // 🔍 New search functionality
+  setSearchTerm: (term) => set({ searchTerm: term }),
 }));
