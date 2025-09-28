308bed68def309e53cbd8a40df0615ad444a2cd6
stellalagat <stellalagat058@gmail.com>
2025-09-28 23:45:10 +0300

    app.jsx

--- tailwind-react-integration/app.jsx
+++ tailwind-react-integration/app.jsx
@@ -1,7 +1,12 @@
+import UserProfile from "./components/UserProfile";
+
 export default function App() {
   return (
-    <h1 className="text-4xl font-bold text-green-500">
-      Tailwind is Working! 🎉
-    </h1>
-  )
+    <div className="p-6">
+      <h1 className="text-4xl font-bold text-green-500 mb-6">
+        Tailwind is Working! 🎉
+      </h1>
+      <UserProfile />
+    </div>
+  );
 }
