8cf9d246edca5fd8df1e472d6294fb25eb94833e
stellalagat <stellalagat058@gmail.com>
2025-10-02 09:44:54 +0300

    style react

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
-        🚀 Tailwind is working!
+        🚀 Tailwind is working in Recipe Sharing Platform!
       </h1>
     </div>
   )
